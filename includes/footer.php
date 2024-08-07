f7cd1208404f7d46b3cc993170feceddf65fb8e3
Cristhian <Cristhian.452@outlook.com>
2024-08-07 00:09:07 -0500

    Dependencias corregisdas, y footer con links

--- includes/footer.php
+++ includes/footer.php
@@ -1,14 +1,19 @@
 <footer class="bg-body-tertiary text-center text-lg-start">
     <div class="text-center p-3 bg-dark">
         <p class="text-white mb-0">&copy; 2024 Derechos Reservados NORLIAJEFF</p>
-        <a class="text-white" href="https://github.com/liangreyes12" target="_blank" rel="noopener noreferrer">
-            <i class="fab fa-github"></i> GitHub
-        </a>
+        <div class="footer-links">
+            <a class="text-white" href="https://github.com/liangreyes12" target="_blank" rel="noopener noreferrer">
+                <i class="fab fa-github"></i> Liang_Reyes
+            </a>
+            <a class="text-white" href="https://github.com/Cristhianm30" target="_blank" rel="noopener noreferrer">
+                <i class="fab fa-github"></i> Cristhian_Moreno
+            </a>
+        </div>
     </div>
 </footer>
 
 <!-- Incluir Font Awesome para iconos -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha384-k6RqeWeci5ZR/Lv4MR0sA0FfDOMM8M3frvY9RVZpvD1ZGL47U9JueNyxs8ssjBYU" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
 <!-- Incluir jQuery desde CDN -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> 
